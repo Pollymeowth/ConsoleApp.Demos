e687b063f8a308658d3534efd191edff27e9bf46
Pollyana de Sousa <pollyana.de.sousa@ericsson.com>
2025-04-25 14:40:35 -0300

    Add project files.

---
+++ ConsoleApp.OutputDemo/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! My name is Pollyana Sousa");
+Console.WriteLine("I am from Brazil");
+Console.WriteLine("Anything you want to say");
---
+++ ConsoleApp.VariablesDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string name = "Pollyana";
+
+Console.WriteLine("I am " + name); //string concatenation   
+Console.WriteLine($"They call me {name}");// string interpolation
+Console.WriteLine("They call me {0}", name);//formatted string
+
+int age = 30;
+int retirementYearsAge = 34;
+int retirementAge = age + retirementYearsAge;
+Console.WriteLine($"Hello! My name is {name} and I am {age} years old.");
+Console.WriteLine("My retirement age is:" + retirementAge);
+
+
+bool isRetired = false;
+
+if ( age > retirementYearsAge)
+{
+    isRetired = true;
+    Console.WriteLine("Você é aposentado!");
+}
+else
+{ 
+    isRetired = false;
+    Console.WriteLine($"{name} não é aposentado!");
+}
+
